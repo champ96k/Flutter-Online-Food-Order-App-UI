82c14addb3cdbafe58274b9bcdfe1aad714d9040
Tushar Nikam <champ96k@gmail.com>
2020-04-03 10:08:04 +0530

    intial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:online_food_order_app_ui/ui/HomePage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
